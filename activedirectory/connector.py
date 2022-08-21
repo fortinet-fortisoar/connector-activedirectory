b22a3cf86c21c0e443a0d51291ba2c3c15e62338
cs-sujeet-ahirrao <sahirrao@fortinet.com>
2022-08-21 12:09:28 +0530

    Added Pluggable Enrichment Playbook
    Added Pluggable Enrichment Playbook

--- activedirectory/connector.py
+++ activedirectory/connector.py
@@ -1,11 +1,13 @@
 """ Copyright start
-  Copyright (C) 2008 - 2021 Fortinet Inc.
+  Copyright (C) 2008 - 2022 Fortinet Inc.
   All rights reserved.
   FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
   Copyright end """
 
 from connectors.core.connector import Connector, get_logger, ConnectorError
 from .operations import operations, _check_health
+from connectors.cyops_utilities.builtins import make_cyops_request
+from .constant import MACRO_LIST
 
 logger = get_logger("activedirectory")
 
@@ -24,4 +26,26 @@ class ActiveDirectory(Connector):
             return _check_health(config)
         except Exception as err:
             raise ConnectorError('{}'.format(str(err)))
+
+    def del_micro(self, config):
+        for macro in MACRO_LIST:
+            try:
+                resp = make_cyops_request(f'/api/wf/api/dynamic-variable/?name={macro}', 'GET')
+                if resp['hydra:member']:
+                    macro_id = resp['hydra:member'][0]['id']
+                    resp = make_cyops_request(f'/api/wf/api/dynamic-variable/{macro_id}/?format=json', 'DELETE')
+            except Exception as e:
+                logger.error(e)
+
+    def on_deactivate(self, config):
+        self.del_micro(config)
+
+    def on_activate(self, config):
+        self.del_micro(config)
+
+    def on_add_config(self, config, active):
+        self.del_micro(config)
+
+    def on_delete_config(self, config):
+        self.del_micro(config)
 
